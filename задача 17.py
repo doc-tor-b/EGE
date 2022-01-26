9c6439b830379743f6ea6379994c359236c9c9cd
Doctor <masemennikov@gmail.com>
2022-01-26 20:50:37 +0300

    Исправил ошибку в подсчёте комбинаций в 17 задаче

--- задача 17.py
+++ задача 17.py
@@ -1,4 +1,4 @@
-import math
+# Слишком умный способ подходит больше для 27 задачи с похожим условием
 
 f = open("17.txt", "r")
 array = [0] * 126
@@ -15,15 +15,18 @@ for number in f:
         max_array[n % 126] = n
 
 f.close()
+
 count = 0
-# где-то здесь ошибка в подсчёте количества комбинаций
 for i in range(1, 126 // 2):
     if array[i] != 0 and array[126 - i] != 0:
         local_count = array[i] * array[126 - i]
         count += local_count
-if array[0] > 0:
-    count += math.factorial(array[0]) / (math.factorial(array[0] - 2))
 
+if array[0] > 0:
+    count += array[0] * (array[0] - 1) / 2
+
+if array[63] != 0:
+    count += array[63] * (array[63] - 1) / 2
 max_sum = 0
 
 for i in range(1, 126):
@@ -34,5 +37,4 @@ if max_array[0] != 0 and max_array[126] != 0:
     max_sum = max(max_sum, max_array[0] + max_array[126])
 
 print("count", count)
-print(array)
 print("max_sum", max_sum)
